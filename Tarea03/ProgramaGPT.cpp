fda179ab5bbbf54dbfb8ad633f58d263169725e8
Edgar Chalico <edgar1569@outlook.com>
2023-09-23 18:38:53 -0600

    Modificaciones Tarea03

--- Tarea03/ProgramaGPT.cpp
+++ Tarea03/ProgramaGPT.cpp
@@ -6,8 +6,7 @@
 
 namespace po = boost::program_options;
 
-// Función para dibujar una circunferencia por el algoritmo de Bresenham
-void drawCircle(int radius)
+void drawCircle(int radius, int centerX, int centerY)
 {
 	glPointSize(1.0);
 	glBegin(GL_POINTS);
@@ -18,22 +17,15 @@ void drawCircle(int radius)
 
 	while (x >= y)
 	{
-		// Octante 1
-		glVertex2i(x, y);
-		// Octante 2
-		glVertex2i(y, x);
-		// Octante 3
-		glVertex2i(-y, x);
-		// Octante 4
-		glVertex2i(-x, y);
-		// Octante 5
-		glVertex2i(-x, -y);
-		// Octante 6
-		glVertex2i(-y, -x);
-		// Octante 7
-		glVertex2i(y, -x);
-		// Octante 8
-		glVertex2i(x, -y);
+		// Calcular las coordenadas en relación al centro
+		glVertex2i(centerX + x, centerY + y);
+		glVertex2i(centerX + y, centerY + x);
+		glVertex2i(centerX - y, centerY + x);
+		glVertex2i(centerX - x, centerY + y);
+		glVertex2i(centerX - x, centerY - y);
+		glVertex2i(centerX - y, centerY - x);
+		glVertex2i(centerX + y, centerY - x);
+		glVertex2i(centerX + x, centerY - y);
 
 		y++;
 
@@ -53,17 +45,37 @@ void drawCircle(int radius)
 
 int main(int argc, char **argv)
 {
-	// Declarar variables para el radio y el valor predeterminado
-	int radio;
-	int radioPredeterminado = (3 * std::min(800, 600)) / 8;
+	// Declarar variables para las coordenadas (x, y) del centro del círculo, con valores predeterminados (0, 0)
+	int x = 0;
+	int y = 0;
+	int radio = (3 * std::min(800, 600)) / 8;
 
-	// Configurar las opciones de línea de comandos
-	po::options_description desc("Opciones");
-	desc.add_options()("radio,r", po::value<int>(&radio)->default_value(radioPredeterminado), "Radio del círculo");
+	try
+	{
+		// Configurar las opciones de línea de comandos
+		po::options_description desc("Opciones");
+		desc.add_options()("help", "Mostrar ayuda")("x", po::value<int>(&x)->default_value(0), "Coordenada X del centro del círculo")("y", po::value<int>(&y)->default_value(0), "Coordenada Y del centro del círculo")("radio,r", po::value<int>(&radio)->default_value(radio), "Radio del círculo");
 
-	po::variables_map vm;
-	po::store(po::parse_command_line(argc, argv, desc), vm);
-	po::notify(vm);
+		po::variables_map vm;
+		po::store(po::parse_command_line(argc, argv, desc), vm);
+		po::notify(vm);
+
+		// Verificar si se proporcionó el argumento "help"
+		if (vm.count("help"))
+		{
+			std::cout << desc << std::endl; // Mostrar instrucciones
+			return 0;
+		}
+
+		// Resto del código como se indicó anteriormente
+		// ...
+	}
+	catch (const po::error &ex)
+	{
+		std::cerr << "Error al procesar los argumentos: " << ex.what() << std::endl;
+		std::cout << "Utiliza --help para ver las instrucciones." << std::endl;
+		return 1;
+	}
 
 	// Inicializar GLFW
 	if (!glfwInit())
@@ -99,9 +111,9 @@ int main(int argc, char **argv)
 	{
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		// Dibujar la circunferencia con el radio especificado
+		// Dibujar la circunferencia con las coordenadas (x, y) especificadas y el radio especificado
 		glColor3f(1.0, 1.0, 1.0); // Color blanco
-		drawCircle(radio);
+		drawCircle(radio, x, y);
 
 		// Detectar si la tecla ESC está siendo presionada
 		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
